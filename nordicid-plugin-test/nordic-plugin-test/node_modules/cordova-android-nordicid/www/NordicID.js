a4616fec0d79b1029f6d8a1e93ca3a1c13bcdd03
Ivo Gonçalves <kaly.imsg@gmail.com>
2019-10-11 01:26:21 +0200

    Does notr buiuld yet

---
+++ nordicid-plugin-test/nordic-plugin-test/node_modules/cordova-android-nordicid/www/NordicID.js
@@ -0,0 +1,9 @@
+var exec = require('cordova/exec');
+
+exports.setReadingStrength = function (success, error, level) {
+    exec(success, error, 'NordicID', 'setReadingStrength', level);
+};
+
+exports.getReadingStrength = function (success, error) {
+    exec(success, error, 'NordicID', 'getReadingStrength', null);
+};
--- nordicid-plugin-test/nordic-plugin-test/platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ nordicid-plugin-test/nordic-plugin-test/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,6 +1,16 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-  module.exports = [];
+  module.exports = [
+    {
+      "id": "cordova-android-nordicid.NordicID",
+      "file": "plugins/cordova-android-nordicid/www/NordicID.js",
+      "pluginId": "cordova-android-nordicid",
+      "clobbers": [
+        "NordicID"
+      ]
+    }
+  ];
   module.exports.metadata = {
-    "cordova-plugin-whitelist": "1.3.4"
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-android-nordicid": "1.0.0"
   };
 });
