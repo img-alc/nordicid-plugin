9b50c4b73fbe68560f6d0b98bb82b2d89c5e5fe0
Ivo Gonçalves <kaly.imsg@gmail.com>
2019-10-11 02:02:57 +0200

    removed test project

--- nordicid-plugin-test/nordic-plugin-test/node_modules/cordova-android-nordicid/www/NordicID.js
+++
@@ -1,9 +0,0 @@
-var exec = require('cordova/exec');
-
-exports.setReadingStrength = function (success, error, level) {
-    exec(success, error, 'NordicID', 'setReadingStrength', level);
-};
-
-exports.getReadingStrength = function (success, error) {
-    exec(success, error, 'NordicID', 'getReadingStrength', null);
-};
